d5536c7fcbf0409a201f36917319af296aa32600
Zain ul Abideen <zaiinn440@gmail.com>
2023-09-16 10:10:51 +0500

    Update pre_processing.py

--- preprocessing/pre_processing.py
+++ preprocessing/pre_processing.py
@@ -53,6 +53,17 @@ def clahe_equalized(imgs):
     return imgs_equalized
 
 
+# ===== normalize over the dataset
+def dataset_normalized(imgs):
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[1]==1)  #check the channel is 1
+    imgs_normalized = np.empty(imgs.shape)
+    imgs_std = np.std(imgs)
+    imgs_mean = np.mean(imgs)
+    imgs_normalized = (imgs-imgs_mean)/imgs_std
+    for i in range(imgs.shape[0]):
+        imgs_normalized[i] = ((imgs_normalized[i] - np.min(imgs_normalized[i])) / (np.max(imgs_normalized[i])-np.min(imgs_normalized[i])))*255
+    return imgs_normalized
 
 
 def adjust_gamma(imgs, gamma=1.0):
